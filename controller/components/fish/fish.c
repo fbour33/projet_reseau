9479ed2fb42f7e511ab4a8a4c383e49bea0a5fdd
Tago Brice <btago@bordeaux-inp.fr>
2023-03-24 18:36:28 +0100

    modif on fish structure

--- controller/components/fish/fish.c
+++ controller/components/fish/fish.c
@@ -1,13 +1,14 @@
 #include "fish.h"
 #include <string.h>
 
-struct fish * create_fish(enum FISH_TYPE fish_type,enum STRATEGY strategy,int x_start,int y_start,int x_final,int y_final){
+struct fish * create_fish(enum FISH_TYPE fish_type,enum STRATEGY strategy,int x,int y,int width,int height){
     struct fish * fish= malloc(sizeof(struct fish));
 
     fish->strategy=strategy;
     fish->type=fish_type;
-    fish->start_position=_position(x_start,y_start);
-    fish->final_position=_position(x_final,y_final);
+    fish->position=_position(x,y);
+    fish->rectangle.width=width;
+    fish->rectangle.height=height;
 
     return fish;
 }
--- controller/components/fish/fish.h
+++ controller/components/fish/fish.h
@@ -6,10 +6,12 @@
 
 
 struct fish{
+    char *name;
+    int running;
     enum FISH_TYPE type;
     enum STRATEGY strategy;
-    struct position start_position;
-    struct position final_position;
+    struct position position;
+    struct dimension rectangle;
 };
 
 /**
@@ -17,13 +19,13 @@ struct fish{
  * @brief           create a fish using the given value for the parameters
  * @param fish_type the type of the fish to be created
  * @param strategy  the moving strategy of the fish to be created
- * @param x_star    the x depature coordinates of the fish
- * @param y_start   the y depature coordinates of the fish
- * @param x_final   the x final coordinates of the fish
- * @param y_final   the y final coordinates of the fish
+ * @param x         the x coordinates of the fish
+ * @param y         the y coordinates of the fish
+ * @param width     the width of the fish
+ * @param height    the height of the fish
  * @return          the adress of the strcuture fish created
  */
-struct fish * create_fish(enum FISH_TYPE fish_type,enum STRATEGY strategy,int x_start,int y_start,int x_final,int y_final);
+struct fish * create_fish(enum FISH_TYPE fish_type,enum STRATEGY strategy,int x,int y,int width,int height);
 
 /**
  * @fn          free_fish
