5e820088759e80bb764a7c43f551dc24dfba6d74
Alexandre Bertin <abertin002@enseirb-matmeca.fr>
2023-03-26 15:13:46 +0200

    debug du dernier commit de brice

--- controller/components/view/view.c
+++ controller/components/view/view.c
@@ -3,8 +3,8 @@
 struct view *create_view(int id, int x, int y, int width, int height){
     struct view * view = malloc(sizeof(struct view));
 
-    view.id = id;
-    view.p = _position(x,y);
-    view.width = width;
-    view.height = height;
+    view->id = id;
+    view->p = _position(x,y);
+    view->d.width = width;
+    view->d.height = height;
 }
