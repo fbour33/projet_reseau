53945d20eb15af19b1fe9ae3d1e6ce16ef32b483
Jules Gauthier <jgauthier010@bordeaux-inp.fr>
2023-05-24 09:45:20 +0200

    pull

--- affichage/test_afficheur.java
+++ affichage/test_afficheur.java
@@ -6,78 +6,58 @@ public class test_afficheur {
 
     public static void main(String[] args) {
         String serverAddress = "localhost";
-        int port = 5000;
+        int port = 3000;
 
         try {
             
             // Établissement de la connexion avec le serveur
             Socket socket = new Socket(serverAddress, port);
 
-            OutputStream outputStream = null;
-            PrintWriter writer = null;
-            InputStream inputStream = null;
-            BufferedReader reader = null;
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+            PrintWriter writer = new PrintWriter(outputStream, true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-             // Envoi du message de connexion au serveur
-            outputStream = socket.getOutputStream();
-            writer = new PrintWriter(outputStream, true);
-            writer.println("hello in as N3");
+            // Envoi du message de connexion au serveur
+            writer.println("hello");
 
-            // Lecture de la réponse du serveur
-            inputStream = socket.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
+            // Lecture de la réponse du serveur       
             String response = reader.readLine();
             System.out.println("Server response: " + response);
 
              // Envoi du message d'ajout d'un poisson
-            outputStream = socket.getOutputStream();
-            writer = new PrintWriter(outputStream, true);
-            writer.println("getFishes");
+            writer.println("ls");
 
             // Lecture de la réponse du serveur
-            inputStream = socket.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
             response = reader.readLine();
             System.out.println("Server response: " + response);
 
 
             // Envoi du message d'ajout d'un poisson
-            outputStream = socket.getOutputStream();
-            writer = new PrintWriter(outputStream, true);
-            writer.println("addFish CloWNFish at 40x30,10x3, RandomWaypoint");
+            writer.println("addFish ClownFish_0 at 40x30,10x3, RandomWaypoint");
 
             // Lecture de la réponse du serveur
-            inputStream = socket.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
             response = reader.readLine();
             System.out.println("Server response: " + response);
 
             // Envoi du message d'ajout d'un poisson
-            outputStream = socket.getOutputStream();
-            writer = new PrintWriter(outputStream, true);
-            writer.println("addFish ClownFish_3 at 40x30,10x3, RandomWaypoint");
+            writer.println("addFish ClownFish_1 at 70x30,10x3, RandomWaypoint");
 
             // Lecture de la réponse du serveur
-            inputStream = socket.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
+            response = reader.readLine();
+            System.out.println("Server response: " + response);
+
+            // Envoi du message d'ajout d'un poisson
+            writer.println("addFish ClownFish_2 at 40x30,10x3, RandomWaypoint");
+
+            // Lecture de la réponse du serveur
             response = reader.readLine();
             System.out.println("Server response: " + response);
 
             // Envoi du message d'ajout d'un poisson
             outputStream = socket.getOutputStream();
             writer = new PrintWriter(outputStream, true);
-            writer.println("getFishes");
-
-            // Lecture de la réponse du serveur
-            inputStream = socket.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-            response = reader.readLine();
-            System.out.println("Server response: " + response);
-
-            // Envoi du message d'ajout d'un poisson
-            outputStream = socket.getOutputStream();
-            writer = new PrintWriter(outputStream, true);
-            writer.println("delFish ClownFish_36");
+            writer.println("ls");
 
             // Lecture de la réponse du serveur
             inputStream = socket.getInputStream();
@@ -88,22 +68,38 @@ public class test_afficheur {
             // Envoi du message démarrage d'un poisson
             outputStream = socket.getOutputStream();
             writer = new PrintWriter(outputStream, true);
-            writer.println("startFish CloWNFish");
+            writer.println("startFish ClownFish_1");
 
             // Lecture de la réponse du serveur
             inputStream = socket.getInputStream();
             reader = new BufferedReader(new InputStreamReader(inputStream));
             response = reader.readLine();
             System.out.println("Server response: " + response);
-            
-            
+
+             try{
+                    Thread.sleep(2000);
+                } catch(Exception e) {
+           
+                    // catching the exception
+                    System.out.println(e);
+                }
+
+
+            // Envoi du message démarrage d'un poisson
+            outputStream = socket.getOutputStream();
+            writer = new PrintWriter(outputStream, true);
+            writer.println("ls");
+
+
+
+            /*
             for(int i=0; i<10;i++){
                 
 
                 // Envoi du message d'ajout d'un poisson
                 outputStream = socket.getOutputStream();
                 writer = new PrintWriter(outputStream, true);
-                writer.println("getFishes");
+                writer.println("ls");
 
                 // Lecture de la réponse du serveur
                 inputStream = socket.getInputStream();
@@ -118,17 +114,19 @@ public class test_afficheur {
                     // catching the exception
                     System.out.println(e);
                 }
-            }/*
+            }*/
+
             while(true){
                 // Lecture de la réponse du serveur
                 inputStream = socket.getInputStream();
                 reader = new BufferedReader(new InputStreamReader(inputStream));
                 response = reader.readLine();
                 System.out.println("Server response: " + response);
-                if(response == "bye\n"){
+                if(response == null){
                     break;
                 }
-            }*/
+            }
+
             //Envoi du message de deconnection
             outputStream = socket.getOutputStream();
             writer = new PrintWriter(outputStream, true);
